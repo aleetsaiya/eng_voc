396d566ffdcee37968b8f7b0338c8f6c90e7b4f1
Sean Tsai <aleetsaiya@gmail.com>
2024-06-12 09:07:28 +1000

    implement the basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import data from './voc.json';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Link from '@mui/material/Link';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+    typography: {
+        'fontSize': 16 
+    }
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Container sx={{ maxWidth: 800 }}>
+            <h1>English Vocabulary</h1>
+            <TableContainer component={Paper}> 
+        <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Vocabulary</TableCell>
+            <TableCell align="right">Translated</TableCell>
+            <TableCell align="right" sx={{display: {xs: 'none', md: 'block'}}}>Date</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row, i) => (
+            <TableRow
+              key={i}
+            >
+              <TableCell><Link href={`https://dictionary.cambridge.org/dictionary/english/${row.voc}`} underline='none'>{row.voc}</Link></TableCell>
+              <TableCell align="right">{row.translated}</TableCell>
+              <TableCell align="right" sx={{'fontStyle': 'italic', display: {xs: 'none', md: 'block'}}}>{row.date}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+        </TableContainer>
+        </Container>
+        </ThemeProvider>
+    );
 }
 
 export default App;
+
